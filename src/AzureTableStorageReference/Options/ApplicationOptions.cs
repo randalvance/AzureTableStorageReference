0f38bf5a5cd87b3b59ec81007eff1877a4c455d9
Randal Vance Cunanan <randal@randalvance.net>
2016-10-03 08:35:23 -0400

    Initial Commit

---
+++ src/AzureTableStorageReference/Options/ApplicationOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureTableStorageReference.Options
+{
+    public class ApplicationOptions
+    {
+        public string StorageConnectionString { get; set; }
+    }
+}
---
+++ src/AzureTableStorageReference/Startup.cs
@@ -0,0 +1,49 @@
+﻿using AzureTableStorageReference.Options;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace AzureTableStorageReference
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions();
+            services.AddMvc();
+
+            services.Configure<ApplicationOptions>(Configuration.GetSection("Azure"));
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IOptions<ApplicationOptions> applicationOptions)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
